3f7d826d5a433b1ddcd220b58b81c52cf0488df0
user <user@DESKTOP-U27Q4G0>
2016-10-15 03:41:16 -0400

    Add project files.

---
+++ Computer_Ency/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Computer_Ency.MvcApplication" Language="C#" %>
---
+++ Computer_Ency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Computer_Ency.Startup))]
+namespace Computer_Ency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
