44e080fe31a80e89b60c771a030d308dbc89e359
Paul Steidle <steidlepaul@gmail.com>
2016-10-15 04:00:01 -0400

    did stuff

---
+++ Computer_Ency/Controllers/EncyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Computer_Ency.Controllers
+{
+    public class EncyController : Controller
+    {
+        // GET: Ency
+        public string Index()
+        {
+            return "This is my <b>default</b> action....bitch!";
+        }
+
+        public string Welcome()
+        {
+            return "This is the welcome aciton method";
+        }
+    }
+}
